7e55d1c735786a83ca3811680779897973879ff5
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-16 21:23:13 -0500

    new changes

--- tailwind/02-nike/src/components/Nav.jsx
+++ tailwind/02-nike/src/components/Nav.jsx
@@ -28,6 +28,11 @@ const Nav = () => {
             </li>
           ))}
         </ul>
+        <div className="hidden max-lg:block">
+            <img src={hamburger}
+            width={25}
+            height={25}/>
+        </div>
       </nav>
     </header>
   );
