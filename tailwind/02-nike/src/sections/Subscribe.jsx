fcacbb18d5e9a12d4ef4ef384dde0af74e40ba48
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-20 00:28:40 -0500

    new changes

--- tailwind/02-nike/src/sections/Subscribe.jsx
+++ tailwind/02-nike/src/sections/Subscribe.jsx
@@ -10,7 +10,7 @@ const Subscribe = () => {
         Sign Up for <span className="text-coral-red"> Updates </span>&
         Newsletter
       </h3>
-      <div className="lg-mx-w-[40] w-full flex item-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+      <div className="lg-mx-w-[40%] w-full flex item-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label="Sign Up" fullWidth />
