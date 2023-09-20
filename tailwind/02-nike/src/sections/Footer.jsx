419932e57706fdbfe3cd7909e94aa512e1f982d2
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-20 14:55:03 -0500

    new changes

--- tailwind/02-nike/src/sections/Footer.jsx
+++ tailwind/02-nike/src/sections/Footer.jsx
@@ -1,7 +1,54 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className=" flex flex-col items-start">
+          <a href="/">
+            <img
+              src={footerLogo}
+              alt="logo"
+              width={150}
+              height={46}
+              className="mt-0"
+            />
+          </a>
+          <p className="text-white-400 mt-6 text-base leading-7 font-montserrat sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                className="bg-white flex justify-center items-center w-12 h-12 rounded-full"
+                key={icon.alt}
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
 
-export default Footer
+        <div>
+          {footerLinks.map((section) => {
+            <div key={section.title}>
+              <h4 className="text-white">{section.title}</h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li key={link.name} className="text-white-400">
+                    <a href={link.name}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>;
+          })}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
